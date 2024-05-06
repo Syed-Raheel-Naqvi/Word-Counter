6b65be2041906c34b5dcf725bfdb4de783863420
Syed Raheel Naqvi <raheelhaider654@gmail.com>
2024-05-07 00:03:30 +0500

    created Word Counter

---
+++ index.ts
@@ -0,0 +1,33 @@
+#! /usr/bin/env node
+
+//Import the 'inquirer' module, which is a command line interface for Node.js 
+import inquirer from "inquirer";
+import chalk from "chalk";
+
+console.log(chalk.bold.italic.cyanBright("\n\t\t WORD COUNTER \n"));
+
+// Declare a constant  'awnser' and assign it to the result of inquirer.prompt function
+
+const answers: {
+    Sentence: string
+} = await inquirer.prompt([
+    {
+        name: "Sentence",
+        type: "input",
+        message: chalk.bold("Enter your sentence to count the words: ")
+    }
+]);
+
+const words = answers.Sentence.trim().split(" ")
+
+//print the array of words to the console
+//console.log(words);
+
+if (!answers.Sentence) {
+    console.log(chalk.bold.red("\nThere were no sentence entered: Please enter the sentence!"));
+}
+
+//Print the word count of the sentence to the console
+else {
+    console.log(chalk.greenBright.bold(`\n\tYour sentence word count is: `) + chalk.yellowBright(`${words.length}`));
+}
